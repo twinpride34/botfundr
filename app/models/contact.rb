56cc3375ae55db121392801cb6f8c855f151c025
Max Carlson <maxwell.m.carlson@gmail.com>
2016-05-22 15:25:15 -0700

    updated tab title to FundrBot from BotFundr

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -7,7 +7,7 @@ class Contact < MailForm::Base
   def headers  
     {
       :subject => "Contact Form",
-      :to => "mackenzie@unicasts.com",
+      :to => "max@fundrbot.com",
       :from => %("#{name}" <#{email}>)
      }
   end
